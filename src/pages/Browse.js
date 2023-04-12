ed30ad19a1f895b42d37281ee5e9cf9bc31c091d
Carter Awbrey <94002456+cawbrey@users.noreply.github.com>
2023-04-12 13:30:51 -0500

    added search bar but didn't implement the sorting

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -3,15 +3,15 @@ import StoreItem from "../components/StoreItem";
 import {useContext} from "react";
 import {ShoppingCartContext} from "../context/ShoppingCartContext";
 
-export default function Browse(){
+export default function Browse() {
     const {cart} = useContext(ShoppingCartContext);
     const {changeCart} = useContext(ShoppingCartContext);
     console.log(cart);
 
-    function setQuantity(item, newQuantity){
+    function setQuantity(item, newQuantity) {
         const updatedCart = cart.map((cartItem) => {
             if (cartItem.id === item.id) {
-                return { ...cartItem, quantity: newQuantity };
+                return {...cartItem, quantity: newQuantity};
             }
             return cartItem;
         });
@@ -20,12 +20,21 @@ export default function Browse(){
     };
 
     return (
-      <>
-          <Row sm={1} md={2} lg={3} xxl={4} className="p-5">
-              {cart.map((item) => (
-                  StoreItem(item, (newQuantity) => setQuantity(item, newQuantity))
-              ))}
-          </Row>
-      </>
+        <>
+            <div className="input-group rounded pt-5 px-5" >
+                <input type="search" className="form-control rounded" placeholder="Search"/>
+                <span className="input-group-text border-0">
+                    <img src="./search.svg" alt=""/>
+                </span>
+            </div>
+
+            <Row sm={1} md={2} lg={3} xxl={4} className="p-5">
+
+
+                {cart.map((item) => (
+                    StoreItem(item, (newQuantity) => setQuantity(item, newQuantity))
+                ))}
+            </Row>
+        </>
     );
 }
